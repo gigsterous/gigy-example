2255e787e4af145a3588dbc396e610f439e6ba82
Martin Myslik <m.myslik@gmail.com>
2017-03-25 20:18:20 +0100

    Fix security startup exception

--- src/main/java/com/gigy/config/WebSecurityConfig.java
+++ src/main/java/com/gigy/config/WebSecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
 @EnableWebSecurity
+@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -23,6 +26,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/login");
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/gigy/**")
+				.authorizeRequests().anyRequest().authenticated();
 	}
 
 	@Bean
